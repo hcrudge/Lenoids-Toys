573039ab0a8c3b11728fd177225c72ebee98b752
hcrudge <hcrudge1@gmail.com>
2021-05-27 15:03:17 -0500

    newToys

--- leonids-toys.js
+++ leonids-toys.js
@@ -29,7 +29,30 @@ const toys =[
         ageRecommendation: "Ages 7 & Above"
     }
 ]
-for(const toy of toys) {
-    console.log(toy.name);
-}
-// console.log(toys);
+// Append New Toys
+
+const puzzle =
+    {
+        id: 4,
+        name: "StarWars Puzzle 1000pc",
+        description: "Classic Jigsaw Puzzle",
+        category: "Puzzle",
+        inStock: true,
+        price: 10.99,
+        ageRecommendation: "Ages 12 & Above"
+    }
+    const electronic =
+    {
+        id: 5,
+        name: "Simon Game",
+        description: "Game of Lights and Sounds",
+        category: "Electronic",
+        inStock: false,
+        price: 19.99,
+        ageRecommendation: "Ages 5 & Above"
+    }
+
+toys.push(puzzle, electronic);
+for (const toy of toys) {
+    console.log(toy.name)
+} 
